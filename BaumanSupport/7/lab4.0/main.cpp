14fed4dffac2a41dacddc00c19b551ae13dfec75
0-02 <9x02@mail.ru>
2021-01-13 21:40:54 +0300

    попытка сделать решение методом гомори(неуд)

--- BaumanSupport/7/lab4.0/main.cpp
+++ BaumanSupport/7/lab4.0/main.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
 #include <vector>
+#include "simplex.cpp"
 
-#include "game.h"
-
-void print(const std::vector<double>& strategy, const std::string& player) {
-  std::cout << "Strategy for player " << player << ":" << std::endl;
-  for (size_t i = 0; i < strategy.size(); ++i) {
-    if (i == 0) {
-      std::cout << "F " << std::setw(6) << std::fixed << std::setprecision(2)
-                << strategy[i] << std::endl;
-    } else {
-      std::cout << "x" << i << std::setw(6) << std::fixed
-                << std::setprecision(2) << strategy[i] << std::endl;
-    }
-  }
-}
 
 int main() {
   size_t n = 0;
   size_t m = 0;
   std::cout << "Enter m and n:" << std::endl;
-  std::cin >> m >> n;
-  std::vector<std::vector<double>> game_matrix(m, std::vector<double>(n));
+  /*std::cin >> m >> n;*/
+  n = 3,m=3;
+  std::vector<std::vector<double>> gomoriMatrix(m, std::vector<double>(n));
+  std::vector<std::vector<double>> b={{7},{8},{6}},c={{5,6,4}};
   std::cout << "Enter matrix (as in task):" << std::endl;
-  for (size_t i = 0; i < m; ++i) {
+  /*for (size_t i = 0; i < m; ++i) {
     for (size_t j = 0; j < n; ++j) {
-      std::cin >> game_matrix[i][j];
+      std::cin >> gomoriMatrix[i][j];
     }
-  }
-
-  Game game(game_matrix);
-  auto strategy_A = game.find_strategy_A();
-  auto strategy_B = game.find_strategy_B();
-  print(strategy_A, "A");
-  print(strategy_B, "B");
-
+  }*/
+  gomoriMatrix={{1,1,1},{1,3,0},{0,0.5,4}};
+  SimplexTable sTable = SimplexTable(gomoriMatrix, b,c);
+  
   return 0;
 }
